c2158223fb3733ca781e67197da4462fe8ca33b7
LisaTaldir <lisa.taldir@free.fr>
2024-04-29 21:31:56 +0200

    Optimizations on solve_jacobi
    - put the loop k in last position
    - compute 1/pow(...) one time

--- src/stencil/solve.c
+++ src/stencil/solve.c
@@ -11,24 +11,30 @@ void solve_jacobi(mesh_t* A, mesh_t const* B, mesh_t* C) {
     usz const dim_x = A->dim_x;
     usz const dim_y = A->dim_y;
     usz const dim_z = A->dim_z;
-    for (usz k = STENCIL_ORDER; k < dim_z - STENCIL_ORDER; ++k) {
+
+    f64 tmp;
+
+    for (usz i = STENCIL_ORDER; i < dim_x - STENCIL_ORDER; ++i) {
         for (usz j = STENCIL_ORDER; j < dim_y - STENCIL_ORDER; ++j) {
-            for (usz i = STENCIL_ORDER; i < dim_x - STENCIL_ORDER; ++i) {
+            for (usz k = STENCIL_ORDER; k < dim_z - STENCIL_ORDER; ++k) {
                 C->cells[i][j][k].value = A->cells[i][j][k].value * B->cells[i][j][k].value;
 
                 for (usz o = 1; o <= STENCIL_ORDER; ++o) {
+
+                    tmp = 1/pow(17.0, (f64)o);
+
                     C->cells[i][j][k].value += A->cells[i + o][j][k].value *
-                                               B->cells[i + o][j][k].value / pow(17.0, (f64)o);
+                                               B->cells[i + o][j][k].value * tmp;
                     C->cells[i][j][k].value += A->cells[i - o][j][k].value *
-                                               B->cells[i - o][j][k].value / pow(17.0, (f64)o);
+                                               B->cells[i - o][j][k].value * tmp;
                     C->cells[i][j][k].value += A->cells[i][j + o][k].value *
-                                               B->cells[i][j + o][k].value / pow(17.0, (f64)o);
+                                               B->cells[i][j + o][k].value * tmp;
                     C->cells[i][j][k].value += A->cells[i][j - o][k].value *
-                                               B->cells[i][j - o][k].value / pow(17.0, (f64)o);
+                                               B->cells[i][j - o][k].value * tmp;
                     C->cells[i][j][k].value += A->cells[i][j][k + o].value *
-                                               B->cells[i][j][k + o].value / pow(17.0, (f64)o);
+                                               B->cells[i][j][k + o].value * tmp;
                     C->cells[i][j][k].value += A->cells[i][j][k - o].value *
-                                               B->cells[i][j][k - o].value / pow(17.0, (f64)o);
+                                               B->cells[i][j][k - o].value * tmp;
                 }
             }
         }
