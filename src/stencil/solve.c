b6577d1be1df7ed1262017eeef465177630eb115
Martin DIAMANT <martin.diamant@ens.uvsq.fr>
2024-05-01 14:44:11 +0200

    Reapplied modifications on solve_jacobi
    Reapplided modifications that were lost during a merge

--- src/stencil/solve.c
+++ src/stencil/solve.c
@@ -47,27 +47,33 @@ void solve_jacobi(mesh_t* A, mesh_t const* B, mesh_t* C) {
     //   o_values[o - 1] = 1 / square_and_multiply(17.0, o);
     // }
 
+    f64 o_values[STENCIL_ORDER];
+    for (usz o = 1; o <= STENCIL_ORDER; ++o)
+    {
+      o_values[o - 1] = 1 / pow(17.0, (f64)o);
+    }
     f64 tmp;
     for (usz i = STENCIL_ORDER; i < dim_x - STENCIL_ORDER; ++i)  {
         for (usz j = STENCIL_ORDER; j < dim_y - STENCIL_ORDER; ++j) {
             for (usz k = STENCIL_ORDER; k < dim_z - STENCIL_ORDER; ++k){
-                C->cells[i][j][k].value = A->cells[i][j][k].value * B->cells[i][j][k].value;
+                tmp = A->cells[i][j][k].value * B->cells[i][j][k].value;
 
                 for (usz o = 1; o <= STENCIL_ORDER; ++o) {
-                    tmp = 1 / pow(17.0, (f64)o);
-                    C->cells[i][j][k].value += A->cells[i + o][j][k].value *
-                                               B->cells[i + o][j][k].value * tmp;
-                    C->cells[i][j][k].value += A->cells[i - o][j][k].value *
-                                               B->cells[i - o][j][k].value * tmp;
-                    C->cells[i][j][k].value += A->cells[i][j + o][k].value *
-                                               B->cells[i][j + o][k].value * tmp;
-                    C->cells[i][j][k].value += A->cells[i][j - o][k].value *
-                                               B->cells[i][j - o][k].value * tmp;
-                    C->cells[i][j][k].value += A->cells[i][j][k + o].value *
-                                               B->cells[i][j][k + o].value * tmp;
-                    C->cells[i][j][k].value += A->cells[i][j][k - o].value *
-                                               B->cells[i][j][k - o].value * tmp;
+                    tmp += A->cells[i + o][j][k].value *
+                            B->cells[i + o][j][k].value * o_values[o - 1];
+                    tmp += A->cells[i - o][j][k].value *
+                            B->cells[i - o][j][k].value * o_values[o - 1];
+                    tmp += A->cells[i][j + o][k].value *
+                            B->cells[i][j + o][k].value * o_values[o - 1];
+                    tmp += A->cells[i][j - o][k].value *
+                            B->cells[i][j - o][k].value * o_values[o - 1];
+                    tmp += A->cells[i][j][k + o].value *
+                            B->cells[i][j][k + o].value * o_values[o - 1];
+                    tmp += A->cells[i][j][k - o].value *
+                            B->cells[i][j][k - o].value * o_values[o - 1];
                 }
+
+                C->cells[i][j][k].value = tmp;
             }
         }
     }
